e8ee9784eac9483a01ae576080bba315683694de
Saurabh2386 <sonisourabh2386@gmail.com>
2024-12-15 19:48:39 +0530

    NewJava project

---
+++ JavaPrograms/src/ArithmeticLogics.java
@@ -0,0 +1,7 @@
+public class ArithmeticLogics {
+    public static void sum (int a, int b){
+        System.out.println("1st Value- > "+ a);
+        System.out.println("2nd Value- > "+ b);
+        System.out.println("SuM value = " +(a+b));
+    }
+}
---
+++ JavaPrograms/src/printHello.java
@@ -0,0 +1,17 @@
+public class printHello {
+    public static void printTheHelloWord() {
+        System.out.println("Or kinyaan");
+        printHello2();
+    }
+
+    public static void printHello2() {
+        System.out.println("Or kinyaan2");
+        printHello3();
+
+    }
+
+    public static void printHello3() {
+        System.out.println("Or kinyaan3");
+        ArithmeticLogics.sum(2, 4);
+    }
+}
